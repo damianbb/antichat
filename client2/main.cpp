09219525d97e6d259c173c5316bfe4d99898dcf5
Then <bsmagacz@hotmail.com>
2015-09-25 13:49:57 +0200

    fix 'terminal'

--- client2/main.cpp
+++ client2/main.cpp
@@ -9,11 +9,13 @@ c_UDPasync network_api("fc44:f31b:cae7:5b26:ee5c:f8f3:d2ab:69e7", 12321, 55555);
 
 void __recieve () {
 	while (true) {
-		if (network_api.HasMessages()) {
-            cout << network_api.PopMessage() << endl;
+
+        if (network_api.HasMessages()) {
+            cout << network_api.PopMessage() << endl << "#> ";
+            cout.flush();
 		}
 
-		this_thread::sleep_for(chrono::milliseconds(100));
+        this_thread::sleep_for(chrono::milliseconds(100));
 	}
 }
 
@@ -21,8 +23,8 @@ void __send () {
 	string msg, reply;
 	while (true) {
         cout << "#> ";
-		getline(cin, msg);
-		network_api.Send(msg);
+        getline(cin, msg);
+        network_api.Send(msg);
 	}
 }
 

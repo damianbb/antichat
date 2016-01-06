e1ae290e44b58781d856759a23303c5a16efba75
pign <pign>
2016-01-06 13:35:52 +0100

    client2 refactoring

--- client2/main.cpp
+++ client2/main.cpp
@@ -2,37 +2,38 @@
 #include "c_UDPasync.hpp"
 #include <chrono>
 #include <thread>
+#include <string>
 
 using namespace std;
 using namespace std::chrono;
 c_UDPasync network_api("192.168.0.105", 12321, 55555);
 
-void __recieve () {
-    while (true) {
+void _receive () {
+  while (true) {
 
-        if (network_api.HasMessages()) {
-            cout << network_api.PopMessage() << endl << "#> ";
-            cout.flush();
-        }
-
-        this_thread::sleep_for(chrono::milliseconds(100));
+    if (network_api.HasMessages()) {
+      cout << network_api.PopMessage() << endl << "#> ";
+      cout.flush();
     }
+
+    this_thread::sleep_for(chrono::milliseconds(100));
+  }
 }
 
-void __send () {
-    string msg, reply;
-    while (true) {
-        cout << "#> ";
-        getline(cin, msg);
-        network_api.Send(msg);
-    }
+void _send () {
+  string msg;
+  while (true) {
+    cout << "#> ";
+    getline(cin, msg);
+    network_api.Send(msg);
+  }
 }
 
 int main (int argc, char **argv) {
-    thread odbjur(__recieve);
-    thread wyslij(__send);
+  thread t_receive(_receive);
+  thread t_send(_send);
 
-    odbjur.join();
-    wyslij.join();
-    return 0;
+  t_receive.join();
+  t_send.join();
+  return 0;
 }
